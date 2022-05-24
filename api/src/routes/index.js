1bfdae2cace74f266443d1d19caca9eb38c48957
Arsene <arseneblavier46@gmail.com>
2022-05-24 18:57:33 -0300

    agregando el preload que no anda

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,8 +4,15 @@ const { Op } = require('../db.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
+
+(() => {
+    try {
+        preLoadDb();
+    } catch (error) {
+        console.log(error);
+    }
+})();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
