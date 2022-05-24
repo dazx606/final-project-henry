e57f7a97eb845e133ae39c5a96a0262ef54fbcae
Renato Zapata <renatozapgon@gmail.com>
2022-05-24 13:01:02 -0400

    Modelo de Reservas creado, agregado modelo a inyeccion de sequelize, ruta de filtrados WIP

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { Car, CarType } = require('../db.js')
 const { Op } = require('../db.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -8,6 +9,37 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/cars', async (req, res, next) => {
+    const { model, carType } = req.query
+
+    try {
+        if (model) {
+            const cars = await Car.findAll({ where: { model: { [Op.eq]: model } } })
+            return res.send(cars)
+        }
+        else if (carType) {
+            const cars = await CarType.findAll(
+                {
+                    where: { name: { [Op.eq]: carType } },
+                    include: {
+                        model: Car,
+                        attributes: ['name'],
+                        through: {
+                            attributes: []
+                        }
+                    }
+                })
+                return res.send(cars)
+        }
+        else{
+            const cars = await Car.findAll()
+            return res.send(cars)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 module.exports = router;
