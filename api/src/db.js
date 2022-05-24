4d2bef021b45edb3c26ffa527040135c309f5d0c
Arsene <arseneblavier46@gmail.com>
2022-05-24 14:21:00 -0300

    un pequeno ajuste en db.js

--- api/src/db.js
+++ api/src/db.js
@@ -58,9 +58,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Car, CarType, Location } = sequelize.models;
+const { Car, CarType, Location, IncludedEquipment } = sequelize.models;
 
 // Aca vendrian las relaciones
+
 // Product.hasMany(Reviews);
 
 module.exports = {
