5816ce92d2b5973f18daea6106a683185ea570a7
Arsene <arseneblavier46@gmail.com>
2022-05-24 16:20:03 -0300

    sacando la ruta de Renato

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,36 +10,5 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get('/cars', async (req, res, next) => {
-    const { model, carType } = req.query
-
-    try {
-        if (model) {
-            const cars = await Car.findAll({ where: { model: { [Op.eq]: model } } })
-            return res.send(cars)
-        }
-        else if (carType) {
-            const cars = await CarType.findAll(
-                {
-                    where: { name: { [Op.eq]: carType } },
-                    include: {
-                        model: Car,
-                        attributes: ['name'],
-                        through: {
-                            attributes: []
-                        }
-                    }
-                })
-                return res.send(cars)
-        }
-        else{
-            const cars = await Car.findAll()
-            return res.send(cars)
-        }
-    } catch (error) {
-        next(error)
-    }
-})
-
 
 module.exports = router;
