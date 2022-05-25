8ea97e9cd50f5ede52f5b1d62a5926a8c08ca7a3
Renato Zapata <renatozapgon@gmail.com>
2022-05-25 14:23:51 -0400

    Rutas de filtrado y ordenamiento del back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,30 +1,82 @@
+const { Router } = require('express');
+const { Car, CarType, Location } = require('../db.js')
 const { Op } = require('../db.js');
-const { Router } = require("express");
-const { Car, Location } = require("../db.js");
-
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get("/locations", async (req, res) => {
-  const locations = await Location.findAll({
-    attributes: ["city", "id", "latitude", "longitude"],
-  });
-  res.json(locations);
-});
+router.get('/cars/:locationId', async (req, res, next) => {
+    const { brand, category, order, orderType, date, page} = req.query
+    //PENDIENTE: FILTRADO POR FECHA!
+    //date(availableDate)
+    const { locationId } = req.params
 
-router.get("/locationCars", async (req, res) => {
-  const { locationId } = req.query;
-  const cars = await Car.findAll({
-    where: {
-      location: locationId,
-    },
-  });
-  res.json(cars);
-});
+    try {
+        //los tengo como un array de objetos.
+        
+        if(category && brand){
+            const filterByBoth = await Car.findAll({ 
+                where: {locationId: locationId, brand: brand, category: category},
+                limit: 6,
+                offset: page * 6,
+                order: [[orderType, order]],
+                include: [{model: CarType}]
+            })
+            return res.json(filterByBoth)
+        }
+        if(brand){
+            const carsByBrand = await Car.findAll({ 
+                where: {locationId: locationId, brand: brand},
+                limit: 6,
+                offset: page * 6,
+                order: [[orderType, order]],
+                include: [{model: CarType}]
+            })
+            return res.json(carsByBrand)
+        }
+        if(category){
+            // const type = await CarType.findOne({where: {name: category}})
+            const carsByCategory = await Car.findAll({ 
+                where: {locationId: locationId, category: category},
+                limit: 6,
+                offset: page * 6,
+                order: [[orderType, order]],
+                include: [{model: CarType}]
+            })
+            return res.json(carsByCategory)
+        }
+        if(orderType){
+            const onlyOrder = await Car.findAll({ 
+                where: {locationId: locationId},
+                limit: 6,
+                offset: page * 6,
+                order: [[orderType, order]],
+                include: [{model: CarType}]
+            })
+            return res.json(onlyOrder)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/locationCars/:locationId', async (req, res, next) => {
+    const { locationId } = req.params
+    try {
+        const allCars = await Car.findAll({ 
+            where: {locationId: locationId},
+            include: [{model: CarType}]
+        })
+        return res.json(allCars)
+    } catch (error) {
+        next(error)
+    }
+})
+
 
 module.exports = router;
