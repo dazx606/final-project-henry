3a7d54acb8d16c3bf1416c342acfaff2e5a8ec5e
Arsene <arseneblavier46@gmail.com>
2022-05-26 14:23:25 -0300

    preload de los equipamientos

--- api/src/preloadDb/cars.js
+++ api/src/preloadDb/cars.js
@@ -60,47 +60,47 @@ const generateCars = () => {
                 engine,
                 images,
                 rating: Math.floor(Math.random() * (500 - 100) + 100) / 100,
-                ratingNum: Math.ceil(Math.random()*100),
+                ratingNum: Math.ceil(Math.random() * 100),
                 includedEquipment,
                 opcionalEquipment,
             })
         }
     }
     //---------------------el array de imagenes recibe [imagenTarjeta, imagenImponente, etc...]--------------------
-    addNewCar(10, "Luxury", "Porsche", "Carrera 911", 2021, 600, 2, "small", 9.3, 3, [], [], []);
-    addNewCar(10, "Luxury", "Nissan", "GTR", 2020, 550, 4, "small", 14.3, 3.8, [], [], []);
-    addNewCar(12, "Luxury", "Mercedes", "A-45", 2020, 380, 4, "small", 6.9, 2, [], [], []);
-    addNewCar(12, "Luxury", "BMW", "M4", 2022, 380, 4, "small", 10.4, 3, [], [], []);
-    addNewCar(12, "Luxury", "Audi", "RS 3", 2021, 380, 4, "small", 11.3, 2.5, [], [], []);
-    addNewCar(30, "Premium", "Mazda", "6", 2020, 80, 5, "medium", 6.2, 2, [], [], []);
-    addNewCar(30, "Premium", "Ford", "Fusion", 2020, 80, 5, "medium", 12.2, 2, [], [], []);
-    addNewCar(30, "Premium", "VW", "Jetta", 2021, 80, 5, "medium", 9.4, 2.5, [], [], []);
-    addNewCar(30, "Premium", "Ford", "Mustang", 2020, 95, 4, "small", 9.5, 5, [], [], []);
-    addNewCar(30, "Premium", "Mercedes", "C-180", 2021, 110, 5, "medium", 6.4, 2, [], [], []);
-    addNewCar(30, "Premium", "BMW", "320i", 2022, 110, 5, "medium", 7.3, 2, [], [], []);
-    addNewCar(30, "Premium", "Audi", "A-4", 2022, 110, 5, "medium", 11.5, 2, [], [], []);
-    addNewCar(30, "Pick-up", "Nissan", "Frontier", 2020, 130, 5, "large", 12.1, 2.5, [], [], []);
-    addNewCar(30, "Pick-up", "Toyota", "Hilux", 2021, 130, 5, "large", 9.6, 2.8, [], [], []);
-    addNewCar(30, "Pick-up", "Renault", "Alaskan", 2020, 130, 5, "large", 12.1, 2.5, [], [], []);
-    addNewCar(30, "Pick-up", "VW", "Amarok", 2021, 130, 5, "large", 10.2, 2, [], [], []);
-    addNewCar(30, "Pick-up", "Ford", "Raptor", 2020, 150, 5, "large", 13.4, 3.5, [], [], []);
-    addNewCar(30, "Van", "Mercedes", "Sprinter", 2020, 350, 12, "large", 8.4, 3, [], [], []);
-    addNewCar(30, "Van", "Dodge", "Grand caravan", 2021, 150, 7, "large", 14.1, 3.6, [], [], []);
-    addNewCar(30, "Van", "Renault", "Trafic", 2020, 250, 12, "large", 7.3, 1.6, [], [], []);
-    addNewCar(30, "Van", "Honda", "Odyssey", 2021, 180, 7, "large", 10.2, 3.6, [], [], []);
-    addNewCar(30, "Hybrid", "Toyota", "Rav 4", 2022, 150, 5, "large", 3.5, 2.5, [], [], []);
-    addNewCar(30, "Hybrid", "Kia", "Niro", 2022, 150, 5, "large", 4.4, 1.6, [], [], []);
-    addNewCar(30, "Hybrid", "Toyota", "Corolla", 2020, 120, 5, "medium", 4.5, 1.6, [], [], []);
-    addNewCar(30, "Hybrid", "Suzuki", "Swift", 2020, 80, 5, "small", 3.8, 1.2, [], [], []);
-    addNewCar(30, "SUV Full-Size", "Toyota", "4 Runner", 2021, 130, 7, "large", 12.5, 4, [], [], []);
-    addNewCar(30, "SUV Full-Size", "Nissan", "Patrol", 2022, 180, 7, "large", 14.5, 5.6, [], [], []);
-    addNewCar(30, "SUV Full-Size", "Lexus", "GX 460", 2021, 200, 7, "large", 15.2, 4.6, [], [], []);
-    addNewCar(30, "SUV Full-Size", "Ford", "Explorer", 2021, 140, 5, "large", 8.4, 3, [], [], []);
-    addNewCar(30, "SUV Full-Size", "Jeep", "Rubicon", 2020, 150, 5, "large", 9.6, 3.6, [], [], []);
-    addNewCar(30, "SUV", "Nissan", "Murano", 2022, 95, 5, "large", 11.2, 3.5, [], [], []);
-    addNewCar(30, "SUV", "Mazda", "CX-9", 2022, 95, 5, "large", 11.2, 2.5, [], [], []);
-    addNewCar(30, "SUV", "Chevrolet", "Blazer RS", 2021, 115, 5, "large", 15, 3.6, [], [], []);
-    addNewCar(30, "SUV", "Dodge", "Durango GT", 2020, 145, 7, "large", 10.4, 3.6, [], [], []);
+    addNewCar(10, "Luxury", "Porsche", "Carrera 911", 2021, 600, 2, "small", 9.3, 3, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS"], []);
+    addNewCar(10, "Luxury", "Nissan", "GTR", 2020, 550, 4, "small", 14.3, 3.8, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS"], []);
+    addNewCar(12, "Luxury", "Mercedes", "A-45", 2020, 380, 4, "small", 6.9, 2, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS"], []);
+    addNewCar(12, "Luxury", "BMW", "M4", 2022, 380, 4, "small", 10.4, 3, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS"], []);
+    addNewCar(12, "Luxury", "Audi", "RS 3", 2021, 380, 4, "small", 11.3, 2.5, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS"], []);
+    addNewCar(30, "Premium", "Mazda", "6", 2020, 80, 5, "medium", 6.2, 2, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Premium", "Ford", "Fusion", 2020, 80, 5, "medium", 12.2, 2, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Premium", "VW", "Jetta", 2021, 80, 5, "medium", 9.4, 2.5, ["Manual Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Premium", "Ford", "Mustang", 2020, 95, 4, "small", 9.5, 5, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Premium", "Mercedes", "C-180", 2021, 110, 5, "medium", 6.4, 2, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Premium", "BMW", "320i", 2022, 110, 5, "medium", 7.3, 2, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Premium", "Audi", "A-4", 2022, 110, 5, "medium", 11.5, 2, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Pick-up", "Nissan", "Frontier", 2020, 130, 5, "large", 12.1, 2.5, ["Manual Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Pick-up", "Toyota", "Hilux", 2021, 130, 5, "large", 9.6, 2.8, ["Manual Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Pick-up", "Renault", "Alaskan", 2020, 130, 5, "large", 12.1, 2.5, ["Manual Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Pick-up", "VW", "Amarok", 2021, 130, 5, "large", 10.2, 2, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Pick-up", "Ford", "Raptor", 2020, 150, 5, "large", 13.4, 3.5, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Van", "Mercedes", "Sprinter", 2020, 350, 12, "large", 8.4, 3, ["Manual Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Van", "Dodge", "Grand caravan", 2021, 150, 7, "large", 14.1, 3.6, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Van", "Renault", "Trafic", 2020, 250, 12, "large", 7.3, 1.6, ["Manual Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Van", "Honda", "Odyssey", 2021, 180, 7, "large", 10.2, 3.6, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Hybrid", "Toyota", "Rav 4", 2022, 150, 5, "large", 3.5, 2.5, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Hybrid", "Kia", "Niro", 2022, 150, 5, "large", 4.4, 1.6, ["Manual Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Hybrid", "Toyota", "Corolla", 2020, 120, 5, "medium", 4.5, 1.6, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "Hybrid", "Suzuki", "Swift", 2020, 80, 5, "small", 3.8, 1.2, ["Manual Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV Full-Size", "Toyota", "4 Runner", 2021, 130, 7, "large", 12.5, 4, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV Full-Size", "Nissan", "Patrol", 2022, 180, 7, "large", 14.5, 5.6, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV Full-Size", "Lexus", "GX 460", 2021, 200, 7, "large", 15.2, 4.6, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV Full-Size", "Ford", "Explorer", 2021, 140, 5, "large", 8.4, 3, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV Full-Size", "Jeep", "Rubicon", 2020, 150, 5, "large", 9.6, 3.6, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV", "Nissan", "Murano", 2022, 95, 5, "large", 11.2, 3.5, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV", "Mazda", "CX-9", 2022, 95, 5, "large", 11.2, 2.5, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV", "Chevrolet", "Blazer RS", 2021, 115, 5, "large", 15, 3.6, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
+    addNewCar(30, "SUV", "Dodge", "Durango GT", 2020, 145, 7, "large", 10.4, 3.6, ["Automatic Transmission", "Radio", "Airconditioning", "Electric Windows", "Bluetooth", "Power steering"], ["GPS","Child seat","Baby seat"], []);
 
     if (allCars.length > amountOFCarInLocations) console.log("Too many cars and not enough space in Locations");
     return allCars;
@@ -120,8 +120,8 @@ const aFewHardcodedCars = [
         consumption: 9.3,
         engine: 3,
         images: [],
-        rating:3,
-        location:"Córdoba"
+        rating: 3,
+        location: "Córdoba"
     },
     {
         carType: "Luxury",
@@ -135,8 +135,8 @@ const aFewHardcodedCars = [
         consumption: 14.3,
         engine: 3.8,
         images: [],
-        rating:4, 
-        location:"Córdoba"
+        rating: 4,
+        location: "Córdoba"
     },
     {
         carType: "Luxury",
---
+++ api/src/preloadDb/optionalEquipments.js
@@ -0,0 +1,7 @@
+const optionalEquipments = [
+    "GPS",
+    "Child seat",
+    "Baby seat",
+]
+
+module.exports = { optionalEquipments }
