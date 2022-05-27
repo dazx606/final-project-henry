d16eb60ed1b6df3a0a12692ea0c59224ce624d86
Arsene <arseneblavier46@gmail.com>
2022-05-27 20:48:32 -0300

    rutas arregladas

--- api/src/preloadDb/rentOrders.js
+++ api/src/preloadDb/rentOrders.js
@@ -3,7 +3,7 @@ const rentOrders = [
         startingDate: "2022-08-08",
         endingDate: "2022-08-20",
         user: "unemail1@gmail.com",
-        car: "5",
+        car: "1",
         drivers: ["persona1", "persona2"],
         optionalEquipment: ["GPS"],
         endLocation: "Buenos Aires"
@@ -12,7 +12,7 @@ const rentOrders = [
         startingDate: "2022-10-08",
         endingDate: "2023-02-03",
         user: "unemail2@gmail.com",
-        car: "444",
+        car: "1",
         drivers: ["persona3"],
         optionalEquipment: [],
         endLocation: "Rosario"
@@ -21,7 +21,7 @@ const rentOrders = [
         startingDate: "2022-7-07",
         endingDate: "2022-7-08",
         user: "unemail3@gmail.com",
-        car: "800",
+        car: "1",
         drivers: ["persona5"],
         optionalEquipment: ["GPS", "Child seat", "Baby seat"],
         endLocation: "Cali"
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -11,57 +11,30 @@ router.get('/cars/:locationId', async (req, res, next) => {
   const { brand, category, order, orderType, startDate, endDate, page } = req.query
   //PENDIENTE: FILTRADO POR FECHA!
   //date(availableDate)
-  const { locationId } = req.params
-  const carsPerPage = 8
+  const { locationId } = req.params;
+  const carsPerPage = 8;
 
   const infoFormatter = (info) => {
-    return info.carModels.map(c => {
+    if (!info) return res.status(404).json("No corresponding car found!");
+    let filterdCars = info.carModels.map(c => {
+      const existingRents = [];
+      c.individualCars.forEach(r => {
+        if(r.rentOrders.length) existingRents.push(r.rentOrders);
+      });
       return {
         ...c.dataValues,
         carType: c.carType.dataValues.name,
         includedEquipments: c.includedEquipments.map(e => e.dataValues.name),
         optionalEquipments: c.optionalEquipments.map(e => e.dataValues.name),
+        individualCars: existingRents,
       }
-    });
+    })
+    filterdCars
+    filterdCars = filterdCars.slice((page - 1) * carsPerPage, page * carsPerPage);
+    return res.json(filterdCars);
   }
 
   try {
-  //   let carLocation = await Location.findByPk(
-  //     locationId,
-  //     {
-  //       include: [
-  //         {
-  //           model: CarModel,
-  //           through: { attributes: [] },
-  //           include: [
-  //             { model: CarType },
-  //             { model: IncludedEquipment, attributes: ["name"], through: { attributes: [] } },
-  //             { model: OptionalEquipment, attributes: ["name"], through: { attributes: [] } },
-  //           ]
-  //         },
-  //       ]
-  //     }
-  //   )
-  //   carLocation = carLocation.carModels.map(c => {
-  //     return {
-  //       ...c.dataValues,
-  //       carType: c.carType.dataValues.name,
-  //       includedEquipments: c.includedEquipments.map(e => e.dataValues.name),
-  //       optionalEquipments: c.optionalEquipments.map(e => e.dataValues.name),
-  //     }
-  //   });
-
-  //   if(brand) carLocation = carLocation.filter(c => c.brand === brand);
-  //   if(category) carLocation = carLocation.filter(c => c.carType === category);
-
-  //   if (orderType === "rating") {
-  //     carLocation.sort((a, b) => a.rating - b.rating);
-  //   } else {
-  //     carLocation.sort((a, b) => a.pricePerDay - b.pricePerDay);
-  //   }
-  //   if (order === "DESC") carLocation.reverse();
-
-  //   return res.json(carLocation);
     if (brand && category) {
 
       const locationCarModels = await Location.findByPk(
@@ -70,8 +43,8 @@ router.get('/cars/:locationId', async (req, res, next) => {
           order: [[{ model: CarModel }, orderType, order]],
           include: [
             {
-              model: CarModel, where: { brand: brand }, through: { attributes: [] },  include: [
-                { model: CarType, where: { name: category }, attributes: { exclude: ['id'] }},
+              model: CarModel, where: { brand: brand }, through: { attributes: [] }, include: [
+                { model: CarType, where: { name: category }, attributes: { exclude: ['id'] } },
                 { model: IncludedEquipment, attributes: ['name'], through: { attributes: [] } },
                 { model: OptionalEquipment, attributes: ['name'], through: { attributes: [] } }
               ]
@@ -79,7 +52,7 @@ router.get('/cars/:locationId', async (req, res, next) => {
           ]
         }
       )
-      return res.json(infoFormatter(locationCarModels));
+      return infoFormatter(locationCarModels);
     }
     if (brand) {
       const locationCarModels = await Location.findByPk(
@@ -97,7 +70,7 @@ router.get('/cars/:locationId', async (req, res, next) => {
           ]
         }
       )
-      return res.json(infoFormatter(locationCarModels));
+      return infoFormatter(locationCarModels);
     }
     if (category) {
       const locationCarModels = await Location.findByPk(
@@ -115,28 +88,35 @@ router.get('/cars/:locationId', async (req, res, next) => {
           ]
         }
       )
-      return res.json(infoFormatter(locationCarModels));
+      return infoFormatter(locationCarModels);
     }
-    // if (orderType) {
-    //   const onlyOrder = await CarModel.findAll({
-    //     where: { locationId: locationId },
-    //     limit: carsPerPage,
-    //     offset: page * carsPerPage,
-    //     order: [[orderType, order]],
-    //     include: [
-    //       { model: CarType },
-    //       { model: Location },
-    //       { model: IncludedEquipment, attributes: ["name"], through: { attributes: [] } },
-    //       { model: OptionalEquipment, attributes: ["name"], through: { attributes: [] } },
-    //     ],
-    //   });
-    //   return res.json(onlyOrder);
-    // }
+    if (orderType) {
+      const locationCarModels = await Location.findByPk(
+        locationId,
+        {
+          order: [[{ model: CarModel }, orderType, order]],
+          include: [
+            {
+              model: CarModel, include: [
+                { model: CarType, attributes: { exclude: ['id'] } },
+                { model: IncludedEquipment, attributes: ['name'], through: { attributes: [] } },
+                { model: OptionalEquipment, attributes: ['name'], through: { attributes: [] } },
+                {
+                  model: IndividualCar, attributes: ['id']
+                  , include: [{ model: RentOrder, attributes: ['startingDate', 'endingDate'] }]
+                }
+              ]
+            },
+          ]
+        }
+      )
+      return infoFormatter(locationCarModels);
+    }
   } catch (error) {
     next(error);
   }
 });
-
+//---------------------------------------------------------------------------------------------------------------
 router.get("/locationCars/:locationId", async (req, res, next) => {
   const { locationId } = req.params;
   try {
