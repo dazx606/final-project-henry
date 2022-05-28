70b7d76cb69f809bb580a0391086250c04c9cadc
dazx606 <d.a.z_606@hotmail.com>
2022-05-28 11:46:08 -0500

    se cierra el alert

--- client/src/components/Alert/Alert.jsx
+++ client/src/components/Alert/Alert.jsx
@@ -1,26 +1,45 @@
 
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { showAlert } from '../../redux/actions'
 import LocationFilter from '../LocationFilter/LocationFilter'
 import style from './Alert.module.css'
 
-export default function Alert(props){
+export default function Alert(props) {
     const dispatch = useDispatch()
-    const category = useSelector(state=>state.category);
-    const city = useSelector(state=>state.city);
-    
-    function handleClick(){
+    //const category = useSelector(state=>state.category);
+    const city = useSelector(state => state.city);
+    const [activeBtn, setActiveBtn] = useState(true);
+
+
+    useEffect(() => {
+        if (city !== "") setActiveBtn(false)
+        const element = document.getElementById("All")
+        element.addEventListener("click", (event) => {
+            if (event.target.id === "All") {dispatch(showAlert(true))}
+        });
+    }, [city])
+
+    function handleClick() {
+        
         dispatch(showAlert(true))
     }
+
     
-    return(
-        <div hidden={props.hid} className={style.alertContainer}>
-            <h1 className={style.tittle}>Choose a city to continue</h1>
-            <div className={style.seacrh}>
-                <LocationFilter/>
+    
+    
+
+    return (
+        <div className={style.all} id="All" >
+            <div hidden={props.hid} className={style.alertContainer} id="Alert">
+                <h1 className={style.tittle}>Choose a city to continue</h1>
+                <div className={style.seacrh}>
+                    <LocationFilter />
+                </div>
+                <NavLink to={`/city/${city}`}><button className={style.homeBtn} disabled={activeBtn} onClick={handleClick}>Select</button></NavLink>
             </div>
-            <NavLink to={`/city/${city}`}><button className={style.homeBtn} onClick={handleClick}>Select</button></NavLink>
         </div>
+
     )
 }
