9c6d985698b7d292e6ba48d4a0371e8e247ba4b8
patricia castillo <castillopatriciac@gmail.com>
2022-05-31 16:12:27 -0300

    algunas validaciones de formulario de perfil

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -7,6 +7,7 @@ import styles from './profile.module.css';
 
 export default function Profile() {
     const [input, setInput] = useState({});
+    const [errors, setErrors] = useState({});
     const locations = useSelector((state) => state.locations);
     // const { user } = useAuth0();
     const dispatch = useDispatch()
@@ -33,6 +34,10 @@ export default function Profile() {
             ...input,
             [e.target.name]: e.target.value,
         });
+        setErrors(validations({
+            ...input,
+            [e.target.name]: e.target.value,
+        }))
     }
 
     return (
@@ -42,18 +47,34 @@ export default function Profile() {
                 <div >
                     <div className={styles.titles}>First name: </div>
                     <input className={styles.inputs} type="text" value={input.name} name="name" onChange={(e) => handleChange(e)} />
+                    {
+                        errors.name &&
+                        (<p>{errors.name}</p>)
+                    }
                 </div>
                 <div>
                     <div className={styles.titles}>Last name: </div>
                     <input className={styles.inputs} type="text" value={input.lastName} name="lastName" onChange={(e) => handleChange(e)} />
+                    {
+                        errors.lastName &&
+                        (<p>{errors.lastName}</p>)
+                    }
                 </div>
                 <div>
                     <div className={styles.titles}>Email: </div>
                     < input className={styles.inputs} type="placeholder" value={input.email} name="email" onChange={(e) => handleChange(e)} />
+                    {
+                        errors.email &&
+                        (<p>{errors.email}</p>)
+                    }
                 </div>
                 <div>
                     <div className={styles.titles}>Phone: </div>
                     <input className={styles.inputs} type="text" value={input.phone} name="phone" onChange={(e) => handleChange(e)} />
+                    {
+                        errors.phone &&
+                        (<p>{errors.phone}</p>)
+                    }
                 </div>
                 <div>
                     <div className={styles.titles}>Language: </div>
@@ -64,17 +85,22 @@ export default function Profile() {
                 </div>
                 <div className={styles.titles}>
                     <div>Document Id: </div>
-                    <input className={styles.inputs} type="text" value={input.DocumentId} name="DocumentId" onChange={(e) => handleChange(e)} />
+                    <input className={styles.inputs} type="text" value={input.documentId} name="documentId" onChange={(e) => handleChange(e)} />
+                    {errors.documentId &&
+                        (<p>{errors.documentId}</p>)}
+
                 </div>
                 <div className={styles.titles}>
                     <div>License: </div>
                     <input className={styles.select} type="text" value={input.license} name="license" onChange={(e) => handleChange(e)} />
+                    {errors.license &&
+                        (<p>{errors.license}</p>)}
                 </div>
                 <div >
                     <div className={styles.titles}>City: </div>
 
-                    <select className={styles.select} value={input.city} onChange={handleChange} >
-                        <option hidden>City</option>
+                    <select className={styles.select} value={input.city} name='city' onChange={handleChange} >
+                        <option>City</option>
                         {
                             locations?.map(l =>
                                 <option key={l.city} value={l.id}>{l.city}</option>
@@ -83,9 +109,42 @@ export default function Profile() {
                     </select>
                 </div>
                 <div className={styles.button}>
-                    <button  type="submit">Go</button>
+                    <button type="submit">Go</button>
                 </div>
             </form>
         </div>
     );
+}
+
+function validations(input) {
+    let errors = {}
+
+    if (!input.name) {
+        errors.name = " Name is required"
+    } else if (!/^[a-z A-Z]+$/.test(input.name)) {
+        errors.name = " Name must be letters only"
+    } else if (input.name.length > 15) {
+        errors.name = " Name must be less than 15 characters"
+    }
+    if (!input.lastName) {
+        errors.lastName = " Last Name is required"
+    } else if (!/^[a-z A-Z]+$/.test(input.lastName)) {
+        errors.lastName = " Last Name must be letters only"
+    }
+    if (!input.license) {
+        errors.license = "License is required"
+    } else if (input.license.length !== 10) {
+        errors.license = "License must be at least 10 characters between letters and numbers"
+    }
+    if (!input.documentId) {
+        errors.documentId = "Document Id is required"
+    } else if (input.DocumentId.length !== 15) {
+        errors.documentId = "Document Id must be 9 characters letters o numbers"
+    }
+    if (!input.phone) {
+        errors.phone = "Phone is required"
+    } else if (input.phone.length !== 10) {
+        errors.phone = "Phone must be 10 characters"
+    }
+    return errors
 }
