1221952eb7dece37c32e62f83a7a422f07512ab8
dazx606 <d.a.z_606@hotmail.com>
2022-06-01 14:34:00 -0500

    mauth directory deleted

--- api/src/auth/jwt.js
+++
@@ -1,9 +0,0 @@
-var jwt = require('jsonwebtoken');
-const { user } = require('pg/lib/defaults');
-require('dotenv').config();
-
-const payload = {
-    "id":user.id,
-    "email":user.email,
-    "role":user.admin
-}
