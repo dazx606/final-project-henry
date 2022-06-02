67d7aea545c2318d4f5d3341dcb9cb3977bd1b5a
patricia castillo <castillopatriciac@gmail.com>
2022-06-02 14:17:58 -0300

    rutas del back de get users y delete users

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -1,17 +1,42 @@
 const { Router } = require("express");
+const { User, RentOrder, Driver, Payment } = require("../db.js");
+const { expressjwt: jwt } = require("express-jwt");
+const jwks = require("jwks-rsa");
 
 const router = Router();
 
-
-
-router.get("/", (req,res,next)=>{
-    try {
-        res.send("si")
-    } catch (error) {
-        next(error)
-    }
-    
+router.get("/users", async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      include: [
+        { model: RentOrder, attributes: ["startingDate", "endingDate"] },
+        {
+          model: Driver,
+          attributes: ["firstName", "lastName"],
+        },
+        { model: Payment, attributes: ["firstName", "lastName"] },
+      ],
+    });
+    return res.json(users);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
+router.delete("/users/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    await user.destroy();
+    return res.json({ message: "User deleted" });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 module.exports = router;
