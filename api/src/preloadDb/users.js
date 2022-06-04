98b08942a4150de6901eea34207c4689cac4ac71
patricia castillo <castillopatriciac@gmail.com>
2022-06-04 15:55:53 -0300

    permiso para front

--- api/src/preloadDb/users.js
+++ api/src/preloadDb/users.js
@@ -23,6 +23,11 @@ const users = [
     email: "bea7san@gmail.com",
     admin: true,
     picture: 'https://lh3.googleusercontent.com/a/AATXAJxb4VcfsKKtL3AnvYCzjq6YgLPdhFzYOFDnvN1M=s96-c',
+  },
+  {
+    email: "castillopatricia@gmail.com",
+    admin: true,
+    picture: 'https://lh3.googleusercontent.com/a/AATXAJxkVJbeNB4Pp-92JzDgWagSJojKjfW-LqnxFl8z=s96-c',
   }
 ];
 
