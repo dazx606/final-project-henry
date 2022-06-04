0847a8c6476a42fdc752a40641a5e413261c260c
patricia castillo <castillopatriciac@gmail.com>
2022-06-04 15:58:56 -0300

    conflicto preload

--- api/src/preloadDb/users.js
+++ api/src/preloadDb/users.js
@@ -25,15 +25,12 @@ const users = [
     picture: 'https://lh3.googleusercontent.com/a/AATXAJxb4VcfsKKtL3AnvYCzjq6YgLPdhFzYOFDnvN1M=s96-c',
   },
   {
-<<<<<<< HEAD
     email: "castillopatricia@gmail.com",
     admin: true,
     picture: 'https://lh3.googleusercontent.com/a/AATXAJxkVJbeNB4Pp-92JzDgWagSJojKjfW-LqnxFl8z=s96-c',
-=======
     email: "cristian.achetoni@gmail.com",
     admin: true,
     picture: 'https://lh3.googleusercontent.com/a-/AOh14GiSpc2K2nqllAU_T4s1ABFe1xMTGSLqY8WqOL8NyQ=s96-c',
->>>>>>> 66c4c329909d6cb0ad0dcb2b792dc39e8b4dd4f6
   }
 ];
 
