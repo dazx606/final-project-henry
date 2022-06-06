be7969267f2f63ac0ed6c4879d659e070b62e6eb
Arsene <arseneblavier46@gmail.com>
2022-06-06 14:00:59 -0300

    Fleet en la navbar pero andando

--- client/src/components/Alert/Alert.jsx
+++ client/src/components/Alert/Alert.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { showAlert } from '../../redux/actions'
@@ -6,12 +6,12 @@ import LocationFilter from '../LocationFilter/LocationFilter'
 import style from './Alert.module.css'
 
 export default function Alert() {
-    // const ref = useRef(null);
     const dispatch = useDispatch()
     const city = useSelector(state => state.city);
     const [activeBtn, setActiveBtn] = useState(true);
 
     useEffect(() => {
+        console.log("hola");
         if (city !== "") setActiveBtn(false)
         const element = document.getElementById("All")
         element.addEventListener("click", (event) => {
@@ -26,24 +26,6 @@ export default function Alert() {
     function handleClick() {
         dispatch(showAlert(true))
     }
-
-    // const onClickOutside = () => { dispatch(showAlert(true)) }
-
-    // useEffect(() => {
-    //     const handleClickOutside = (event) => {
-    //         console.log(ref.current);
-    //         console.log(event.target);
-    //         if (ref.current && !ref.current.contains(event.target)) {
-    //             onClickOutside && onClickOutside();
-    //         }
-    //     };
-    //     document.addEventListener('click', handleClickOutside, true);
-    //     return () => {
-    //         document.removeEventListener('click', handleClickOutside, true);
-    //     };
-    // }, [onClickOutside]);
-
-    // if (!props.show) return null;
 
     return (
         <div className={style.all} id="All" >
