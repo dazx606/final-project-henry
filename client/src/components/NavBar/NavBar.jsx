42fe309c91823fc51544a135b9b4d0d7dada66b8
Arsene <arseneblavier46@gmail.com>
2022-06-06 11:21:55 -0300

    nav se cierra automatico

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,6 +6,7 @@ import styles from "./NavBar.module.css";
 
 function NavBar() {
   const [display, setDisplay] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
   const { loginWithPopup, isAuthenticated, user } = useAuth0();
 
   function handleLoginInfo() {
@@ -13,20 +14,24 @@ function NavBar() {
     if (!isAuthenticated) loginWithPopup()
   }
 
+  const showAllLinks = () => {
+    setShowLinks(!showLinks);
+  }
+
   return (
-    <div>
+    <div className={styles.navSuperContainer}>
       <div className={styles.navcontainer}>
-        <input type="checkbox" id="btn-nav" className={styles.checkbox} />
+        <input type="checkbox" id="btn-nav" className={styles.checkbox} checked={showLinks} onChange={() => { }} />
         <header className={styles.header}>
           <div className={styles.headercontainer}>
-            <label htmlFor="btn-nav" className={styles.btnLabel}>
+            <label htmlFor="btn-nav" className={styles.btnLabel} onClick={showAllLinks}>
               <div className={styles.headerButton}></div>
             </label>
             <NavLink className={styles.tittle} to="/"><h1>RENT A CAR</h1></NavLink>
 
             {isAuthenticated ?
               <div onClick={handleLoginInfo} className={styles.logIcon} >
-                <div >{user.given_name ? user.given_name[0].toLocaleUpperCase() :  user.nickname[0].toLocaleUpperCase()}</div>
+                <div >{user.given_name ? user.given_name[0].toLocaleUpperCase() : user.nickname[0].toLocaleUpperCase()}</div>
               </div>
               :
               <div onClick={handleLoginInfo} className={styles.icon} >
@@ -42,21 +47,20 @@ function NavBar() {
         <nav className={styles.menu}>
           <ul className={styles.link}>
             <NavLink to="/">
-              <li className={styles.list}>HOME</li>
+              <li className={styles.list} onClick={showAllLinks}>HOME</li>
             </NavLink>
             <NavLink to="/about">
-              <li className={styles.list}>ABOUT US</li>
+              <li className={styles.list} onClick={showAllLinks}>ABOUT US</li>
             </NavLink>
             <NavLink to="/contact">
-              <li className={styles.list}>CONTACT US</li>
+              <li className={styles.list} onClick={showAllLinks}>CONTACT US</li>
             </NavLink>
             <NavLink to="/booking">
-              <li className={styles.list}>BOOKING</li>
+              <li className={styles.list} onClick={showAllLinks}>BOOKING</li>
             </NavLink>
           </ul>
         </nav>
       </div>
-
       <Outlet />
     </div>
   );
