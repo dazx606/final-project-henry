337c265e0e79ecded2c7978f90545bd35b38fa05
crisar0790 <cristian.achetoni@gmail.com>
2022-06-08 13:57:57 -0300

    cambios en rental Details

--- client/src/pages/RentalDetails/RentalDetails.jsx
+++ client/src/pages/RentalDetails/RentalDetails.jsx
@@ -5,7 +5,7 @@ import { userReservation } from "../../redux/actions";
 import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./RentalDetails.module.css";
 
-export default function RentalDetails() {
+export default function RentalDetails({ match }) {
 
     const reservation = useSelector(state => state.userReservation);
     const dispatch = useDispatch();
@@ -14,63 +14,86 @@ export default function RentalDetails() {
 
     useEffect(() => {
         dispatch(userReservation(getAccessTokenSilently, orderId))
-        console.log(reservation)
-    }, [dispatch, orderId]);
+    }, [dispatch, getAccessTokenSilently, orderId]);
 
     return (
         <div className={styles.rentDetails}>
             <div className={styles.containerRent}>
                 <div className={styles.detailsRent}>DETAILS</div>
-                <div className={styles.itemsRent}>
-                    <div>Reservation Number:</div>
-                    <div>{reservation.order.id}</div>
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Brand Car:</div>
-                    <div>{reservation.order.individualCar.carModel.brand}</div>
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Model Car:</div>
-                    <div>{reservation.order.individualCar.carModel.model}</div>
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Starting Date:</div>
-                    <div>{reservation.order.startingDate}</div>
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Starting Location:</div>
-                    <div>{reservation.order.individualCar.location.city}</div>
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Ending Date:</div>
-                    <div>{reservation.order.endingDate}</div>
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Ending Location:</div>
-                    <div>{reservation.order.location.city}</div>
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Drivers:</div>
-                    {
-                        reservation.order.drivers.map(d => 
-                            <div>{`${d.firstName} ${d.lastName}`}</div>
-                            )
-                    }
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Optional Equipment:</div>
-                    {
-                        reservation.order.optionalEquipments.map(e => 
-                            <div>{e.name}</div>
-                            )
-                    }
-                </div>
-                <div className={styles.itemsRent}>
-                    <div>Status:</div>
-                    <div>{reservation.order.status}</div>
+                <div className={styles.containerRent2}>
+                {
+                    reservation ?
+                        (
+                            <table>
+                                <tbody>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Reservation Number:</td>
+                                        <td className={styles.itemsRent2}>{reservation.order?.id}</td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Brand Car:</td>
+                                        <td className={styles.itemsRent2}>{reservation.order?.individualCar.carModel.brand}</td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Model Car:</td>
+                                        <td className={styles.itemsRent2}>{reservation.order?.individualCar.carModel.model}</td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Starting Date:</td>
+                                        <td className={styles.itemsRent2}>{reservation.order?.startingDate}</td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Starting Location:</td>
+                                        <td className={styles.itemsRent2}>{reservation.order?.individualCar.location.city}</td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Ending Date:</td>
+                                        <td className={styles.itemsRent2}>{reservation.order?.endingDate}</td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Ending Location:</td>
+                                        <td className={styles.itemsRent2}>{reservation.order?.location.city}</td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Drivers:</td>
+                                        <td>
+                                            {
+                                                reservation.order?.drivers.map(d =>
+                                                    <tr>
+                                                        <td className={styles.itemsRent2}>{`${d.firstName} ${d.lastName}`}</td>
+                                                    </tr>
+                                                )
+                                            }
+                                        </td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Optional Equipment:</td>
+                                        <td>
+                                            {
+                                                reservation.order?.optionalEquipments.map(e =>
+                                                    <tr>
+                                                        <td className={styles.itemsRent2}>{e.name}</td>
+                                                    </tr>
+                                                )
+                                            }
+                                        </td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Payment Amount:</td>
+                                        <td className={styles.itemsRent2}>{`$ ${reservation.order?.paymentAmount}`}</td>
+                                    </tr>
+                                    <tr className={styles.itemsRent}>
+                                        <td className={styles.itemsRent1}>Status:</td>
+                                        <td className={styles.itemsRent2}>{reservation.order?.status}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        ) :
+                        <div>Loading...</div>
+                }
                 </div>
             </div>
-            <div>
+            <div className={styles.buttons}>
                 <button className='buttonGlobal'>Modify Order</button>
                 <button className='buttonGlobal'>Cancel Order</button>
             </div>
