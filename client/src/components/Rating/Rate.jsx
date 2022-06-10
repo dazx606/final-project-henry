472d59010f7683042d431a64363dc61ed9bd607d
Bea7San <b.sanchez013@gmail.com>
2022-06-10 14:25:22 -0500

    rating

--- client/src/components/Rating/Rate.jsx
+++ client/src/components/Rating/Rate.jsx
@@ -2,59 +2,67 @@ import React, { useState } from 'react'
 import style from "./Rating.module.css";
 
 function Rate({ dbUser }) {
-    const [rating, setRating] = useState()
+    const [rating, setRating] = useState(0)
     const stars = [1, 2, 3, 4, 5];
-    let fullStars = [ ...Array(rating)];
+    let fullStars = [...Array(rating)];
+    let emptyStars = [...Array(5 - rating)];
     // onClick={() => handleRating(fs)}
 
+    const handleRating = (fs) => {
+        setRating(fs);
+        setTimeout()
+    }
+
     return (
-        <div className={`boxGlobal ${style.popUpcont}`}>
-            <div>Give Feedback!</div>
+        <div id='All' className={style.all}>
+            <div className={`boxGlobal ${style.popUpcont}`}>
+                <div>Give us some feedback!</div>
 
-            <div className={style.info} >
-                <div className={style.question} >How did you enjoy the {dbUser.reservations[0].brand} {dbUser.reservations[0].model}?</div>
-                <img className={style.carImg} src={dbUser.reservations[0].img} />
-                <div className={style.starsCont}>
-                    {
-                        fullStars.length ?
-                            fullStars.map((fs, i) => {
-                                return (
-                                    <div key={i} className={style.stars} >
-                                        <i className="fa-solid fa-star"></i>
-                                    </div>
-                                )
-                            })
-                            : null
-                    }
-                    {
-                        stars.map((s) => {
-                            return (
-                                <div key={s} className={style.stars} >
-                                    <i className="fa-regular fa-star"></i>
-                                </div>
-                            )
-                        })
-                    }
+                <div className={style.info} >
+                    <div className={style.question} >How did you enjoy using our {dbUser.reservations[0].brand} {dbUser.reservations[0].model}?</div>
+                    <img className={style.carImg} src={dbUser.reservations[0].img} />
+                    <div className={style.starsCont}>
+                        {
+                            fullStars.length ?
+                                fullStars.map((fs, i) => {
+                                    return (
+                                        <div key={i} className={style.stars} >
+                                            <i className="fa-solid fa-star"></i>
+                                        </div>
+                                    )
+                                })
+                                :
+                                stars.map((s) => {
+                                    return (
+                                        <div key={s} className={style.stars} onClick={() => handleRating(s)} >
+                                            <i className="fa-regular fa-star"></i>
+                                        </div>
+                                    )
+                                })
+                        }
+                        {
+                            emptyStars.length &&
+                                emptyStars.length !== 5 ?
+                                emptyStars.map((es) => {
+                                    return (
+                                        <div key={es} className={style.stars} >
+                                            <i className="fa-regular fa-star"></i>
+                                        </div>
+                                    )
+                                })
+                                : null
+                        }
+
+                    </div>
+                    <div className={style.btonCont}>
+                        <button className={`buttonGlobal ${style.btonDismiss}`}>Dismiss</button>
+                    </div>
+
                 </div>
-
             </div>
 
-
-
-
-
-
-            {/* {
-                dbUser.reservations?.map((r, i) => {
-
-                    <div key={i} className={style.info} >
-                        {console.log('hola')}
-                        <div className={style.question} >How did you enjoy the {r.brand} {r.model}?</div>
-                        <img src={r.img} />
-                    </div>
-                })
-            } */}
         </div>
+
     )
 }
 
