a14bdf354f2f7dcb3ecb3323b0db0a2ea24e5702
patricia castillo <castillopatriciac@gmail.com>
2022-06-13 17:55:56 -0300

    pasamos a tabla y css responsive de reservations del lado del usuario

--- client/src/components/ProfileOrders/ProfileOrders.jsx
+++ client/src/components/ProfileOrders/ProfileOrders.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import styles from "../../pages/ProfileOptions/ProfileOptions.module.css";
 import { userReservations } from "../../redux/actions";
 import { useAuth0 } from "@auth0/auth0-react";
+import style from './profileOrders.module.css'
 
 export default function ProfileOrders() {
   const dispatch = useDispatch();
@@ -17,7 +18,7 @@ export default function ProfileOrders() {
   }, [dispatch, userInfo.data.id]);
 
   return (
-    <div className={styles.profileinfo}>
+    <div className={style.usersBox}>
       {userInfo.data && (
         <>
           <div
@@ -31,14 +32,14 @@ export default function ProfileOrders() {
               fontSize: ".8rem",
               fontWeight: "700",
               paddingTop: "2em",
-              paddingBottom: "2em", 
+              paddingBottom: "2em",
               color: "var(--color4)"
             }}>
               MY RESERVATIONS
             </span>
           </div>
-          <div>
-            <div>
+          <div >
+            {/* <div>
               <div style={{
                 display: "flex",
                 fontSize: ".8rem",
@@ -112,9 +113,56 @@ export default function ProfileOrders() {
                     </div>
                   )
                   )}
+            </div> */}
+
+            <div className={style.listTitle}>
+              <table className={style.table}>
+                <thead>
+                  <tr>
+                    <th className={style.tableTitle}>Number</th>
+                    <th className={style.tableTitle}>Car</th>
+                    <th className={style.tableTitle}>Starting Date</th>
+                    <th className={style.tableTitle}>Ending Date</th>
+                    <th className={style.tableTitle}>Status</th>
+                    <th className={style.tableTitle}>Detail</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    !reservations.length ?
+                      (<tr>
+                        <th>You don't have any reservations. Go booking!</th>
+                      </tr>) :
+                      reservations?.map((r, k) =>
+                      (
+                        <tr>
+                          <th>
+                            {r.id}
+                          </th>
+                          <th>{`${r.individualCar.carModel.brand} ${r.individualCar.carModelModel}`}</th>
+                          <th>{r.startingDate}</th>
+
+                          <th>{(new Date(new Date((new Date(r.endingDate)).getTime()).setDate(new Date((new Date(r.endingDate)).getTime()).getDate() - 2))).toDateString()}</th>
+
+
+                          <th>{r.status === 'maintenance' ? 'concluded' : r.status}</th>
+
+                          <th>
+                            <NavLink to={`/reservation/${r.id}`}>
+                              <button style={{ background: "var(--color6Tr)", border: "0", cursor: "pointer" }}>
+                                <i className="fa-solid fa-circle-info" style={{ color: "var(--color3)" }}></i>
+                              </button>
+                            </NavLink>
+                          </th>
+
+                        </tr>
+                      )
+                      )}
+
+                </tbody>
+
+              </table>
             </div>
-
-
 
           </div>
         </>
